b997f7e2142734309a57156d700e4c77f978de8b
naeemashhar <naeemashhar98@gmail.com>
2025-06-18 20:19:19 +0530

    Exploring(HTTP-methods)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
 const express = require('express');
 
 const app = express();
-app.use("/",(req, res) =>{
-    res.send('This is the default response for all other routes.');
-})
 
-app.use("/test", (req, res) =>{           //request handler
-    res.send('Welcome to the test page!');
-})
 
-app.use("/admin", (req, res) =>{
+//request handler (Order of Routes matters)
+
+//this will only handel GET requests 
+app.get("/user", (req, res) => {
+    res.send({firstName : "Naeem", lastName: "Ashhar"});
+});
+
+
+
+app.post("/user", (req, res) => {
+    res.send({message: "User created successfully!"});
+});
+
+
+
+app.delete("/user", (req, res) => {
+    res.send({message: "User deleted successfully!"});
+});
+
+
+app.put("/user", (req, res) => {
+    //this will handle PUT requests
+    //PUT is used to update the entire resource
+    res.send({message: "User updated successfully!"});
+});
+
+
+app.patch("/user", (req, res) => {
+    //this will handle PATCH requests
+    //PATCH is used to update a part of the resource
+    res.send({message: "User partially updated successfully!"});
+});
+
+
+
+
+
+
+//this will match all the HTTP methods 
+app.use("/admin", (req, res) =>{//request handler (Order of Routes matters)
     res.send('Welcome to the admin page!');
 })
 
