1df8fc079d29a4a74b1f443420ffb006ad025660
naeemashhar <naeemashhar98@gmail.com>
2025-06-21 17:01:43 +0530

    Error-Handling

--- src/app.js
+++ src/app.js
@@ -2,54 +2,44 @@ const express = require('express');
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");//second way to use middleware
 
 //ALWAYS REMEMBER HOW NODE.JS WORKS(LINE BY LINE MAIN STACK etc .....),ORDER MATTER
 
 
 
+//Error handling 
+//first-way(better way)
+app.get('/user',(req, res, next) => { //another way of using middleware
+    try {
 
-//Why we use middlewares?
-// when (/user) called it will check line by line until he founds the respond and that response call as (REQUEST-HANDLERS) and rest of is called as (MIDDLEWARES).
-//(Get /user => middleware-chain => request-handler.) 
-//Example : 
-
-//handling middleware request for all GET, POST, PUT, DELETE requests to /admin, using (app.use)
-
-
-//this is the first way to use middleware
-/* app.use("/admin",(req, res, next) => { 
-    console.log('check the admin authorization'); 
-    //logic of authorization
-    const token = "xyzasdas";
-    const authorized = token === "xyz";
-    if(!authorized){
-        res.status(401).send('Access Denied');
+        throw new Error('Something went wrong');     
+        res.send('User data sended');
+    } catch (error) {
+       res.status(500).send('Internal Server Error'); 
     }
-    else{
-        next();
-    }
+    
+});
+
+app.get('/product', (req, res, next) => {
+    res.send('Product data sended');
+});
+
+
+//Error handling middleware(at global Route)always keep this at end of all routes
+//second-way(not a good way but keep this as last line of code)
+
+/* app.get('/user',(req, res, next) => { //another way of using middleware
+    //logic of DB call and getting user data
+    
+    throw new Error('Something went wrong');     
+    res.send('User data sended');
 }); */
 
-//second way to use middleware
-app.use("/admin", adminAuth);
-
-
-app.get('/admin/getAllData', (req, res, next) =>{
-    res.send('All Data sended');
-});
-
-app.get('/admin/deleteUser', (req, res, next) =>{
-    res.send('User deleted');
-})
-
-app.post('/user/login', (req, res, next) => { //this will not use middleware
-    res.send('User logged in');
-});
-
-
-app.get('/user/data', userAuth, (req, res, next) => { //another way of using middleware
-    res.send('User data sended');
+app.use("/", (err,req, res, next) => {//err always be first parameter (ORDER-MATTER)
+    if (err) {
+        // Log the error message
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 
